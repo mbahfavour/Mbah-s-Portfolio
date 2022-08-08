dcc8f1465788738d24efd2b08db06506a405008b
Mbah Favour <mbahdivinefavour21@gmail.com>
2022-08-08 18:07:06 +0100

    second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='mt-20'>
-      <div className=' h-40 p-20 text-5xl md:text-7xl font-univers'>
-        <h1 className=' font-extrabold '>software engineer,</h1>
+    <div className='border'>
+      <div className='flex h-40  text-5xl md:text-7xl font-univers'>
+        <h1 className=' font-normal px-20 pt-20 md:p-20'>software engineer,</h1>
+        <div className='border h-60 md:h-60 w-80 md:w-0 md:ml-20'></div>
       </div>
+      <div className='border mt-20 md:mt-20'></div>
+      <div className='border mt-5 md:mt-10'></div>
       
-      <div className='pt-20 pl-20 md:pl-60 text-3xl md:text-5xl ml-40 md:ml-80'>
-        <h1 className='font-bold'>mbah. divinefavour. chiamaka.</h1>
+      <div className='flex  md:pl-60 text-3xl md:text-5xl'>
+        <div className='border h-60 md:h-60 w-80 md:w-0'></div>
+        <h1 className='font-normal md:ml-40 px-20 md:p-20 py-10'>mbah. divinefavour. chiamaka.</h1>
       </div>
     </div>
   )
